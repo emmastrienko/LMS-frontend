e8d48b5080c75061ddc9137d3fa6fc917f978aef
Emma Strienko <mstrienko@gmail.com>
2025-01-07 15:15:47 +0200

    added user login feature

--- redux/features/auth/authApi.ts
+++ redux/features/auth/authApi.ts
@@ -1,5 +1,5 @@
 import { apiSlice } from "../api/apiSlice";
-import { userRegistration } from "./authSlice";
+import { userLoggedIn, userRegistration } from "./authSlice";
 
 type RegistrationResponse = {
   message: string;
@@ -30,19 +30,42 @@ export const authApi = apiSlice.injectEndpoints({
           console.log(error);
         }
       },
-      
     }),
     activation: builder.mutation({
-      query: ({activation_token, activation_code}) => ({
-         url: "activate_user",
-         method: "POST",
-         body: {
+      query: ({ activation_token, activation_code }) => ({
+        url: "activate_user",
+        method: "POST",
+        body: {
           activation_token,
-          activation_code
-         }
-      })
+          activation_code,
+        },
+      }),
+    }),
+    login: builder.mutation({
+      query: ({email, password}) => ({
+        url: "login",
+        method: "POST",
+        body: {
+          email,
+          password
+        },
+        credentials: "include" as const,
+      }),
+      async onQueryStarted(arg, { queryFulfilled, dispatch }) {
+        try {
+          const result = await queryFulfilled;
+          dispatch(
+            userLoggedIn({
+              accessToken: result.data.accessToken,
+              user: result.data.user,
+            })
+          );
+        } catch (error: any) {
+          console.log(error);
+        }
+      },
     })
   }),
 });
 
-export const { useRegisterMutation, useActivationMutation } = authApi;
+export const { useRegisterMutation, useActivationMutation, useLoginMutation } = authApi;

7ec9ce3cfdbbc5ae09f73ecb92c9b46d1a5f6a03
Emma Strienko <mstrienko@gmail.com>
2025-02-10 18:38:09 +0200

    get all users, manage team

---
+++ app/components/Admin/Users/AllUsers.tsx
@@ -0,0 +1,175 @@
+import React, { FC, useState } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import { Box, Button } from "@mui/material";
+import { useTheme } from "next-themes";
+import { AiOutlineDelete, AiOutlineMail } from "react-icons/ai";
+import Loader from "../../Loader/Loader";
+import { format } from "timeago.js";
+import { useGetAllUsersQuery } from "@/redux/features/user/userApi";
+import { styles } from "@/app/styles/style";
+
+type Props = {
+  isTeam: boolean;
+};
+
+const AllUsers: FC<Props> = ({ isTeam }) => {
+  const { theme, setTheme } = useTheme();
+  const [active, setActive] = useState(false);
+
+  const { data, error, isLoading } = useGetAllUsersQuery({});
+
+  const columns = [
+    { field: "id", headerName: "ID", flex: 0.3 },
+    { field: "name", headerName: "Name", flex: 0.5 },
+    { field: "email", headerName: "Email", flex: 0.5 },
+    { field: "role", headerName: "Role", flex: 0.5 },
+    { field: "courses", headerName: "Purchased Courses", flex: 0.5 },
+    { field: "created_at", headerName: "Joined At", flex: 0.5 },
+
+    {
+      field: " ",
+      headerName: "Delete",
+      flex: 0.2,
+      renderCell: (params: any) => {
+        return (
+          <>
+            <Button>
+              <AiOutlineDelete
+                className="dark:text-white text-black"
+                size={20}
+              />
+            </Button>
+          </>
+        );
+      },
+    },
+    {
+      field: "  ",
+      headerName: "Email",
+      flex: 0.2,
+      renderCell: (params: any) => {
+        return (
+          <>
+            <Button>
+              <a href={`mailto:${params.row.email}`}>
+                <AiOutlineMail
+                  className="dark:text-white text-black"
+                  size={20}
+                />
+              </a>
+            </Button>
+          </>
+        );
+      },
+    },
+  ];
+
+  const rows: any = [];
+
+  if (isTeam) {
+    const newData =
+      data && data.users.filter((item: any) => item.role === "admin");
+    if (newData) {
+      newData.forEach((item: any) =>
+        rows.push({
+          id: item._id,
+          name: item.name,
+          email: item.email,
+          role: item.role,
+          courses: item.courses.length,
+          created_at: format(item.createdAt),
+        })
+      );
+    }
+  } else {
+    if (data) {
+      data.users.forEach((item: any) =>
+        rows.push({
+          id: item._id,
+          name: item.name,
+          email: item.email,
+          role: item.role,
+          courses: item.courses.length,
+          created_at: format(item.createdAt),
+        })
+      );
+    }
+  }
+
+  return (
+    <div className="mt-[120px]">
+      {isLoading ? (
+        <Loader />
+      ) : (
+        <Box m="20px">
+          <div className="w-full flex justify-end">
+            <div
+              className={`${styles.button} !w-[200px] dark:bg-[#57c7a3] !h-[35px] dark:border dark:border-[#ffffff6c]`}
+              onClick={() => setActive(!active)}
+            >
+              Add New Member
+            </div>
+          </div>
+          <Box
+            m="40px 0 0 0"
+            height="80vh"
+            sx={{
+              "& .MuiDataGrid-root": {
+                border: "none",
+                outline: "none",
+                "--DataGrid-containerBackground":
+                  theme === "dark" ? "#3e4396" : "#A4A9FC",
+              },
+              "& .css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon": {
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-sortIcon": {
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-row": {
+                color: theme === "dark" ? "#fff" : "#000",
+                borderBottom:
+                  theme === "dark"
+                    ? "1px solid #ffffff30!important"
+                    : "1px solid #ccc!important",
+              },
+              "& .MuiTablePagination-root": {
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-cell": {
+                borderBottom: "none",
+              },
+              "& .name-column--cell": {
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-columnHeaders": {
+                backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
+                borderBottom: "none",
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiDataGrid-virtualScroller": {
+                backgroundColor: theme === "dark" ? "#1F2A40" : "#F2F0F0",
+              },
+              "& .MuiDataGrid-footerContainer": {
+                backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
+                borderTop: "none",
+                color: theme === "dark" ? "#fff" : "#000",
+              },
+              "& .MuiCheckbox-root": {
+                color:
+                  theme === "dark" ? "#b7ebde !important" : "#000 !important",
+              },
+              "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+                color: "#fff !important",
+              },
+            }}
+          >
+            <DataGrid checkboxSelection rows={rows} columns={columns} />
+          </Box>
+        </Box>
+      )}
+    </div>
+  );
+};
+
+export default AllUsers;
